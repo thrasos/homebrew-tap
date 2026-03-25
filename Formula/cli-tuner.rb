2538008cf7255dcd607d1a942099920059c7265a
thrasos <mail@thrasos.net>
2026-03-25 15:06:47 +0200

    Update cli-tuner formula to 0.1.1

--- Formula/cli-tuner.rb
+++ Formula/cli-tuner.rb
@@ -1,8 +1,8 @@
 class CliTuner < Formula
   desc "CLI microphone tuner for bouzouki, tzouras, baglama, and classical guitar"
   homepage "https://github.com/thrasos/cli_tuner"
-  url "https://github.com/thrasos/cli_tuner/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "78b330b890d4c6a89c9a7fe99fae1ccdbad60e95d868a2e5754ff117a9fb061b"
+  url "https://github.com/thrasos/cli_tuner/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "817565d203e28262b04c3192f7e0e31c828ff4208b6cea7a232655267b5be6db"
   license "GPL-3.0-or-later"
 
   depends_on "rust" => :build
